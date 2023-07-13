e65be03d1dcf361173dbc774f8d3aa6a74f9ade3
cyberholics <victorkingoshimua@gmail.com>
2023-07-13 07:26:54 +0100

    service.py

--- service.py
+++ service.py
@@ -1,4 +1,5 @@
 import bentoml
+from bentoml.io import Text
 
 model_ref = bentoml.sklearn.get("naive_bayes_model:latest")
 vectorizer =model_ref.custom_objects['countvectorizer']
@@ -8,12 +9,12 @@ model_runner=model_ref.to_runner()
 svc = bentoml.Service("spam_sms_dectector",runners=[model_runner])
 
 
-@svc.api(input=str,output=str)
+@svc.api(input=Text(),output=Text())
 def classify_sms(message):
     message = message.lower()
     message_list = [message]  
-    encoded_data=vectorizer.transform(message)
-    prediction=model_runner.predict.run(encoded_data)[:, 1]
+    encoded_data=vectorizer.transform(message_list)
+    prediction=model_runner.predict.run(encoded_data)
     result=prediction
     if result >= 0.5:
         return("The SMS is classified as spam.")
